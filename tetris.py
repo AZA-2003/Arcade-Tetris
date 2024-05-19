d308a9e2fdf7c3c24803a2fa1cbbfd90fec4e7f5
SeaSponge <61109988+Squidwarder@users.noreply.github.com>
2024-05-19 11:03:33 -0700

    added diff3, but controls too twitchy

--- tetris.py
+++ tetris.py
@@ -436,11 +436,16 @@ def main():
     EVENT_MOVE_CURRENT_BLOCK = pygame.USEREVENT + 2
     EVENT_UPDATE_DIFF2 = pygame.USEREVENT + 3
     EVENT_MOVE_DIFF2 = pygame.USEREVENT + 4
+    EVENT_UPDATE_DIFF3 = pygame.USEREVENT + 5
+    EVENT_MOVE_DIFF3 = pygame.USEREVENT + 6
     # Speed at which blocks update and move
+    # Lower number means faster
     pygame.time.set_timer(EVENT_UPDATE_CURRENT_BLOCK, 1000)
     pygame.time.set_timer(EVENT_MOVE_CURRENT_BLOCK, 100)
     pygame.time.set_timer(EVENT_UPDATE_DIFF2, 500)
     pygame.time.set_timer(EVENT_MOVE_DIFF2, 50)
+    pygame.time.set_timer(EVENT_UPDATE_DIFF2, 300)
+    pygame.time.set_timer(EVENT_MOVE_DIFF2, 30)
 
     blocks = BlocksGroup()
 
@@ -481,16 +486,24 @@ def main():
 
             try:
                 #* Need to coincide with the update periods 
-                # (so multiples of 5 and 10)
+                #* curr difficulties have periods 1000, 500, 300
+                # (so multiples of 5, 10, 3)
+                #! Controls get extremely twichy at DIFF 2 & 3. 
+                #! May need to make the move events slower
                 if score < 40:
                     if event.type == EVENT_UPDATE_CURRENT_BLOCK:
                         blocks.update_current_block()
                     elif event.type == EVENT_MOVE_CURRENT_BLOCK:
                         blocks.move_current_block()
-                else:
+                elif score < 120:   #* Can also be 90
                     if event.type == EVENT_UPDATE_DIFF2:
                         blocks.update_current_block()
                     elif event.type == EVENT_MOVE_DIFF2:
+                        blocks.move_current_block()
+                else:
+                    if event.type == EVENT_UPDATE_DIFF3:
+                        blocks.update_current_block()
+                    elif event.type == EVENT_MOVE_DIFF3:
                         blocks.move_current_block()
             except TopReached:
                 game_over = True
